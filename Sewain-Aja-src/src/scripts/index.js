ac19e1abd59077eda021163ec1155ef95a717a26
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-07 06:37:40 +0700

    Update index.js

--- Sewain-Aja-src/src/scripts/index.js
+++ Sewain-Aja-src/src/scripts/index.js
@@ -1,1 +1,18 @@
 import 'regenerator-runtime';
+import '../styles/style.css';
+import App from './views/app';
+ 
+const app = new App({
+  button: document.querySelector('.navbar-toggler'),
+  drawer: document.querySelector('#navbarSupportedContent'),
+  content: document.querySelector('#mainContent'),
+  footer: document.querySelector('#footer'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+ 
+window.addEventListener('load', () => {
+  app.renderPage();
+});
