7e0ed1afa0662d329086571f9b0116b62982bf26
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-07 06:22:58 +0700

    Create daftar.js

---
+++ Sewain-Aja-src/src/scripts/views/pages/daftar.js
@@ -0,0 +1,13 @@
+const Daftar = {
+  async render() {
+    return `
+      <h2>daftar Page</h2>
+    `;
+  },
+ 
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+ 
+export default Daftar;
