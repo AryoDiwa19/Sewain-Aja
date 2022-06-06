d537176bbb22d9ec5868e278fdbda4ce4c07d22b
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-07 06:26:28 +0700

    Create konfirmasi_order.js

---
+++ Sewain-Aja-src/src/scripts/views/pages/konfirmasi_order.js
@@ -0,0 +1,13 @@
+const Konfirmasi = {
+	async render() {
+		return `
+			<h2>konfirmasi Page</h2>
+		`;
+	},
+	
+	async afterRender() {
+		// Fungsi ini akan dipanggil setelah render()
+	},
+};
+	
+export default Konfirmasi;
