35504b5d157e2eafdac5462e6c2ca48bbc399a74
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-09 06:40:04 +0700

    Delete sewainaja directory

--- sewainaja/src/scripts/globals/api-endpoint.js
+++
@@ -1,9 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-
-  CARI_KENDARAAN: `${CONFIG.BASE_URL}`,
-
-};
-
-export default API_ENDPOINT;
--- sewainaja/src/scripts/globals/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-
-  BASE_URL: 'https://private-c9196-aryo.apiary-mock.com/users',
-  BASE_IMAGE_URL: '',
-  DEFAULT_LANGUAGE: 'en-us',
-};
-
-export default CONFIG;
