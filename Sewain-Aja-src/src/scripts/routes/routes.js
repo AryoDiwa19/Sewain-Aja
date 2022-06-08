5c0bc1dbe6ca5f734c20cb829028e5cec209f80e
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-09 01:36:57 +0700

    Update routes.js

--- Sewain-Aja-src/src/scripts/routes/routes.js
+++ Sewain-Aja-src/src/scripts/routes/routes.js
@@ -2,19 +2,23 @@ import Home from '../views/pages/home';
 import CariKendaraan from '../views/pages/cari_kendaraan';
 import Laporkan from '../views/pages/laporkan';
 import Sewakan from '../views/pages/sewakan';
+import Bayar from '../views/pages/bayar';
 import Konfirmasi from '../views/pages/konfirmasi_order';
 import Selesai from '../views/pages/selesai_transaksi';
 import Daftar from '../views/pages/daftar';
 import Masuk from '../views/pages/masuk';
+import FAQ from '../views/pages/faq';
 
 const routes = {
   '/': Home, // default page
   '/home': Home,
   '/cari_kendaraan': CariKendaraan,
-  '/konfirmasi_order': Konfirmasi,
-  '/cari_kendaraan/konfirmasi_order/selesai_transaksi': Selesai,
+  '/cari_kendaraan/bayar': Bayar,
+  '/cari_kendaraan/bayar/konfirmasi_order': Konfirmasi,
+  '/cari_kendaraan/bayar/konfirmasi_order/selesai_transaksi': Selesai,
   '/laporkan': Laporkan,
   '/sewakan': Sewakan,
+  '/faq': FAQ,
   '/daftar': Daftar,
   '/masuk': Masuk,
 };
