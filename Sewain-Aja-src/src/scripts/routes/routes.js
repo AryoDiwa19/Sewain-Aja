b18f74b512c078191aca01dbd9ffb2e1f203f210
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-09 09:08:22 +0700

    Update routes.js

--- Sewain-Aja-src/src/scripts/routes/routes.js
+++ Sewain-Aja-src/src/scripts/routes/routes.js
@@ -3,7 +3,7 @@ import CariKendaraan from '../views/pages/cari_kendaraan';
 import Laporkan from '../views/pages/laporkan';
 import Sewakan from '../views/pages/sewakan';
 import Bayar from '../views/pages/bayar';
-import Konfirmasi from '../views/pages/konfirmasi_order';
+import KonfirmasiOrder from '../views/pages/konfirmasi_order';
 import Selesai from '../views/pages/selesai_transaksi';
 import Daftar from '../views/pages/daftar';
 import Masuk from '../views/pages/masuk';
@@ -14,7 +14,7 @@ const routes = {
   '/home': Home,
   '/cari_kendaraan': CariKendaraan,
   '/cari_kendaraan/bayar': Bayar,
-  '/cari_kendaraan/bayar/konfirmasi_order': Konfirmasi,
+  '/cari_kendaraan/bayar/konfirmasi_order': KonfirmasiOrder,
   '/cari_kendaraan/bayar/konfirmasi_order/selesai_transaksi': Selesai,
   '/laporkan': Laporkan,
   '/sewakan': Sewakan,
