640fef8f512fdc1461896f6204018a40261fdcad
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-09 09:03:25 +0700

    Update selesai_transaksi.js

--- Sewain-Aja-src/src/scripts/views/pages/selesai_transaksi.js
+++ Sewain-Aja-src/src/scripts/views/pages/selesai_transaksi.js
@@ -1,13 +1,25 @@
+/* eslint-disable no-tabs */
 const Selesai = {
-	async render() {
-		return `
-			<h2>selesai Page</h2>
+  async render() {
+    return `
+		<div class="container pt-3 pb-5">
+			<div class="card selesai mx-auto p-3">
+				<i class="material-icons text-center mx-auto">done</i>
+				<h3 class="pt-4">Bukti pembayaran Anda telah terkirim.</h3>
+				<p>Silahkan hubungi nomor WhatsApp pemilik untuk pengambilan kendaraan sewa.</p>
+				<div class="button-whatsapp">
+					<a href="">
+						<i class="fa fa-whatsapp p-2 text-center" style="font-size:24px">  Chat Via WhatsApp</i>
+					</a>
+				</div>
+			</div>
+		</div>
 		`;
-	},
-	
-	async afterRender() {
-		// Fungsi ini akan dipanggil setelah render()
-	},
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
 };
-	
+
 export default Selesai;
