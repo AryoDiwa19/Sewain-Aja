f8e79bbe3b91ad03e413e08410bea5f265a96ad0
AryoDiwa19 <057mail2288@gmail.com>
2022-06-12 04:19:53 +0700

    fix login & tambah lapor

--- backend-login/src/dataLogin.js
+++ backend-login/src/dataLogin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const loginUser = express();
-const { db } = require('./model/dbConnection');
+const { dbLogin } = require('./model/dbLogin');
 
 loginUser.use(cors());
 loginUser.use(express.json());
@@ -12,7 +12,7 @@ loginUser.use(bodyParser.urlencoded({ extended: true }));
 loginUser.get('/login/readUser', (req, res) => {
     const sqlQuery = "SELECT * FROM user";
   
-    db.query(sqlQuery, (err, result) => {
+    dbLogin.query(sqlQuery, (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -27,7 +27,7 @@ loginUser.get('/login/readUser', (req, res) => {
     const userPassword = req.body.user_password;
   
     const sqlQuery = "SELECT * FROM user WHERE user_email = ?";
-    db.query(sqlQuery, [userEmail, userPassword], (err, result) => {
+    dbLogin.query(sqlQuery, [userEmail, userPassword], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -45,7 +45,7 @@ loginUser.post('/login/createUser', (req, res) => {
     const userPassword = req.body.user_password;
   
     const sqlQuery = "INSERT INTO user (user_name, user_email, user_password) VALUE (?, ?, ?)";
-    db.query(sqlQuery, [userName, userEmail, userPassword], (err, result) => {
+    dbLogin.query(sqlQuery, [userName, userEmail, userPassword], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -56,8 +56,22 @@ loginUser.post('/login/createUser', (req, res) => {
   });
   // #####  
 
+  // delete
+loginUser.delete('/login/deleteUser', (req, res) => {
+  const userId = req.body.user_id;
+
+  const sqlQuery = "DELETE FROM user WHERE user_id = ?";
+  dbLogin.query(sqlQuery, userId, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+      console.log(result);
+    }
+  });
+});
+// #####
+
   loginUser.listen(8080, () => {
     console.log('server berhasil berjalan pada port 8080!');
   });
-
-  
--- backend-login/src/model/dbLogin.js
+++ backend-login/src/model/dbLogin.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
-const db = mysql.createPool({
+const dbLogin = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'daftar_login'
 });
 
-exports.db = db;
+exports.dbLogin = dbLogin;
